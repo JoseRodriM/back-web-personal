d3a4b24bf4103fa30ed7d718113409b81f62e1e6
JoseRodriM <josearm_1601@hotmail.com>
2022-08-26 00:41:42 +0200

    Actualización

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -6,35 +6,6 @@ require('dotenv').config()
 
 const addClientes = async(req, res)=>{
     const newClient = new cliente(req.body);
-    console.log(req.body)
-    try{
-        if(regexName(newClient.name) == true){
-            if(regexEmail(newClient.email) == true){
-                await newClient.save()
-                transporter.sendMail(mailOptions, function(error, info){
-                    if(error){
-                        console.log(error);
-                    }else{
-                        console.log('Email enviado: ' + info.response)
-                    }
-                })
-                transporter.sendMail(mailHost, function(error, info){
-                    if(error){
-                        console.log(error);
-                    }else{
-                        console.log('Email enviado: ' + info.response)
-                    }
-                })
-            }else{
-                console.log('El Email no es válido')
-            }
-        }else{
-            console.log('El nombre no es válido')
-        }
-    }catch(err){
-        console.log(err)
-    }
-    
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth:{
@@ -50,14 +21,29 @@ const addClientes = async(req, res)=>{
         text : `Hola ${newClient.name}, gracias por escribirnos, te confirmamos que nos ha llegado tu solicitud y será respondida en la brevedad posible, muchas gracias y buen día!!!`
     }
 
-    let mailHost={
-        from:'process.env.GMAIL_USER',
-        to:'process.env.GMAIL_HOST',
-        subject: 'Solicitud informacion',
-        text : `${newClient.name}, ${newClient.email}, ${newClient.message}`
+    console.log(req.body)
+    try{
+        if(regexName(newClient.name) == true){
+            if(regexEmail(newClient.email) == true){
+                await newClient.save()
+                // transporter.sendMail(mailOptions, function(error, info){
+                //     if(error){
+                //         console.log(error);
+                //     }else{
+                //         console.log('Email enviado: ' + info.response)
+                //     }
+                // })
+            .then(response => console.log(response))
+            }else{
+                console.log('El Email no es válido')
+            }
+        }else{
+            console.log('El nombre no es válido')
+        }
+    }catch(err){
+        console.log(err)
     }
-
-
+    res.send()
 }
 
 module.exports =  {addClientes}
